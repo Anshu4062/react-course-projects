01ed0a7bb51d1c4ab1b33b4b3b6184970ed5ad05
Suryansh Singh <supremesofficialyt@gmail.com>
2025-02-27 04:45:13 +0530

    Added dynamic values

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -5,12 +5,15 @@ function genRandomInt(max) {
 }
 
 function Header() {
+
+  const headingReactDesc = reactDescriptions[genRandomInt(2)]
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are going to build!
+        {headingReactDesc} React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
