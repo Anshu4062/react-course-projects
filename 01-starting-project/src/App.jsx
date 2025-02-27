da194e4c261bff4a32f5a9d0089701474953a2a3
Suryansh Singh <supremesofficialyt@gmail.com>
2025-02-27 05:59:30 +0530

    Props-React-Concept

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,21 +1,34 @@
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import reactCoreConceptsImage from "./assets/react-core-concepts.png";
+import componentImg from "./assets/components.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
-
-  const headingReactDesc = reactDescriptions[genRandomInt(2)]
+  const headingReactDesc = reactDescriptions[genRandomInt(2)];
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactCoreConceptsImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {headingReactDesc} React concepts you will need for almost any app you are going to build!
+        {headingReactDesc} React concepts you will need for almost any app you
+        are going to build!
       </p>
     </header>
+  );
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
   );
 }
 
@@ -24,6 +37,31 @@ function App() {
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept
+            title="Components"
+            description="Just a description."
+            image={componentImg}
+          ></CoreConcept>
+          <CoreConcept
+            title="Components"
+            description="Just a description."
+            image={componentImg}
+          ></CoreConcept>
+          <CoreConcept
+            title="Components"
+            description="Just a description."
+            image={componentImg}
+          ></CoreConcept>
+          <CoreConcept
+            title="Components"
+            description="Just a description."
+            image={componentImg}
+          ></CoreConcept>
+        </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
