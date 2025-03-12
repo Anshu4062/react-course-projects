f8a0d5a937cdf53385f1fa6b048aaf4b143a164d
Anshu <supremesofficialyt@gmail.com>
2025-03-12 19:30:41 +0530

    Adding advance Components Examples

--- Section - 2/Components Setting default props/src/components/Examples.jsx
+++ Section - 2/Components Setting default props/src/components/Examples.jsx
@@ -27,7 +27,7 @@ export default function Examples() {
   return (
     <>
       <Section title={"Examples"} id="examples">
-        <Tabs
+        <Tabs ButtonsContainer="menu"
           buttons={
             <>
               <TabButton
